cb6b56d5f20ed000b9605bb4d6af8dca428ad5b0
Mia Tran <maiptran157@gmail.com>
2018-08-20 12:30:29 -0700

    updated idea js from line 6 to line 10 for class Room

--- idea.js
+++ idea.js
@@ -3,7 +3,10 @@
 pull all your ideas for the future of the project in here! 
 it allows us to all have the most current ideas for the project all on git
 
-
-
+Ideas for class Room{}
++add in capacity number this.roomCapacity = 2;
++assume one person check in for their group, that person is this.occupant
++create this.numberOfCustomersInRoom. Set it to the number of people actually staying in the room including the check in person/ this.occupant
++if this.numberOfCustomersInRoom > 2 people then will need to split them into more than 1 room.
 
 */
